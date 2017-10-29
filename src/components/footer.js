a74fa1f9be7bd654967ae7427c0d5e5750bbaf42
fiveinfinity <jlachance1@gmail.com>
2017-10-29 10:04:07 -0500

    adding frog, small styling tweaks and adding links to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -8,11 +8,11 @@ export const Footer = (props) => {
 				<i className="fa fa-copyright" aria-hidden="true"></i>I'M CONVENIENCED, LLC. All rights Reserved.
 			</div>
 			<div className={`${styles.content} ${styles.social}`}>
-				<i className="fa fa-twitter-square fa-inverse" aria-hidden="true"></i>
-				<i className="fa fa-facebook-square fa-inverse" aria-hidden="true"></i>
-				<i className="fa fa-reddit-square fa-inverse" aria-hidden="true"></i>
+				<a href="https://twitter.com/imconvenienced"><i className="fa fa-twitter-square fa-inverse" aria-hidden="true"></i></a>
+				<a href="https://imconvenienced.slack.com/join/shared_invite/enQtMjYyOTMzNjMwOTEyLWMyYTVlZTA1Y2EyZWIyOTI2ZTJmYzdmMTg2MDcwMDAzZjIyODlhZDNjZTFiNjkwMjdhYWJmNzdlZjIyZGYwOWI"><i className="fa fa-slack fa-inverse" aria-hidden="true"></i></a>
+				<a href="https://www.reddit.com/r/imconvenienced/"><i className="fa fa-reddit-square fa-inverse" aria-hidden="true"></i></a>
 			</div>
-			<div className={styles.content}>For Soren.</div>
+			<img className={styles.frog} src="../../public/images/frog.jpg" alt="For Soren." />
 		</div>
 	);
 };
